d44949b80a9f7c6ad4830ec41e5ac0fc41ebe175
Daniel Johnson <dojohnson@ucdavis.edu>
2024-01-22 13:09:17 -0800

    added delete and update functions to lib/prismaFunctions.ts

--- lib/prismaFunctions.ts
+++ lib/prismaFunctions.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -5,6 +7,21 @@ const prisma = new PrismaClient();
 export async function createUser(userData: any) {
   const user = await prisma.user.create({
     data: userData,
+  });
+  return user;
+}
+
+export async function updateUser(userId: string, userData: any) {
+  const user = await prisma.user.update({
+    where: { userId: userId },
+    data: userData,
+  });
+  return user;
+}
+
+export async function deleteUser(userId: string) {
+  const user = await prisma.user.delete({
+    where: { userId: userId },
   });
   return user;
 }
