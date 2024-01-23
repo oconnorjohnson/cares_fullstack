817a8e6c16889f5d955e8948e763cc6630aead3e
Daniel Johnson <dojohnson@ucdavis.edu>
2024-01-22 18:29:02 -0800

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import { ModeToggle } from "@/components/mode-toggle";
 import CurrentUser from "@/components/current-user";
 import ExampleUploader from "@/components/example-uploader";
 import { SignedIn } from "@clerk/nextjs";
+import NavBar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
       <main>
+        <NavBar />
         <ModeToggle />
         <CurrentUser />
         <SignedIn>
