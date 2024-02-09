c29acd419dc222e61c586cb3e9e4ab6aae68ea81
Daniel Johnson <dojohnson@ucdavis.edu>
2024-02-09 14:33:42 -0800

    got requestid via params from dyanmic route, converted from string to number and got request by id via server aciton

--- app/[requestid]/page.tsx
+++ app/[requestid]/page.tsx
@@ -1,9 +1,39 @@
 import { auth } from "@clerk/nextjs";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableFooter,
+} from "@/components/ui/table";
+import { requestRequestByRequestId } from "@/server/actions/request/actions";
 
-const RequestPage = () => {
+const RequestPage = async ({ requestid }: { requestid: string }) => {
+  const requestId = Number(requestid);
+  const request = await requestRequestByRequestId(requestId);
   return (
     <>
-      <div>We'll render the request details in a nice UI here.</div>
+      <div>
+        <Table>
+          <TableCaption>{request.client.first_name}</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter></TableFooter>
+        </Table>
+      </div>
     </>
   );
 };
@@ -11,13 +41,19 @@ const RequestPage = () => {
 export default function RequestDetails({
   params,
 }: {
-  params: { requestId: number };
+  params: { requestid: string };
 }) {
   const { sessionClaims } = auth();
   const isAdmin = (sessionClaims?.publicMetadata as any)?.admin;
   return (
     <>
-      <div>{isAdmin ? <RequestPage /> : <div>Not allowed!</div>}</div>
+      <div>
+        {isAdmin ? (
+          <RequestPage requestid={params.requestid} />
+        ) : (
+          <div>Not allowed!</div>
+        )}
+      </div>
     </>
   );
 }
