51bbf40b4b1f1c4d6cc79b7da6da695ece1641bc
Daniel Johnson <dojohnson@ucdavis.edu>
2024-02-09 16:09:40 -0800

    broke request information into series of cards in column on /[requestid]/page.tsx

--- app/[requestid]/page.tsx
+++ app/[requestid]/page.tsx
@@ -19,23 +19,23 @@ const RequestPage = async ({ requestid }: { requestid: string }) => {
   const request = await requestRequestByRequestId(requestId);
   return (
     <>
-      <div className="flex items-center justify-center py-12">
-        <Card className="w-1/2">
+      <div className="flex flex-col items-center justify-center py-12">
+        <Card className="w-2/3">
           <CardHeader>
             <CardTitle className="flex flex-col text-2xl font-extralight">
               <div className="flex flex-row">
                 <Badge className="font-bold text-lg bg-green-600">
                   {request.user.first_name} {request.user.last_name}&apos;s
                 </Badge>
-                <div className="px-0.5" />
-                request for <div className="px-0.5" />
+                <div className="px-1" />
+                request for
+                <div className="px-1" />
                 <Badge className="font-bold text-lg bg-blue-600">
                   {request.client.first_name} {request.client.last_name}
                 </Badge>
-              </div>
-              <div>
-                made on{" "}
-                <a className="font-bold">
+                <div className="px-1" />
+                made on
+                <a className="pl-1 font-bold">
                   {formatDateWithSuffix(request.createdAt)}.
                 </a>
               </div>
@@ -55,6 +55,128 @@ const RequestPage = async ({ requestid }: { requestid: string }) => {
                 <div className="text-xl font-bold">
                   {request.client.first_name} {request.client.last_name}
                 </div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Agency</div>
+                <div className="text-xl font-bold">{request.agency.name}</div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Status</div>
+                <div className="text-xl font-bold">{request.agency.name}</div>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+        <div className="py-4" />
+        <Card className="w-2/3">
+          <CardHeader>
+            <CardTitle className="flex flex-col text-2xl font-extralight">
+              Details
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="border border-2 py-4 mx-4 rounded-lg">
+            <div className="flex flex-col">{request.details}</div>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+        <div className="py-4" />
+        <Card className="w-2/3">
+          <CardHeader>
+            <CardTitle className="flex flex-col text-2xl font-extralight">
+              Social Determinants of Health
+            </CardTitle>
+            <CardDescription className="text-lg">
+              Selected Social Determinants of Health categories:
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="border border-2 py-4 mx-4 rounded-lg">
+            <div className="flex flex-col">
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">User</div>
+                <div className="text-xl font-bold">
+                  {request.user.first_name} {request.user.last_name}
+                </div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Client</div>
+                <div className="text-xl font-bold">
+                  {request.client.first_name} {request.client.last_name}
+                </div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Agency</div>
+                <div className="text-xl font-bold">{request.agency.name}</div>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+        <div className="py-4" />
+        <Card className="w-2/3">
+          <CardHeader>
+            <CardTitle className="flex flex-col text-2xl font-extralight">
+              Resilient Future Fund Assistance
+            </CardTitle>
+            <CardDescription className="text-lg">
+              Selected categories with which the Resilient Futures Fund can
+              assist:
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="border border-2 py-4 mx-4 rounded-lg">
+            <div className="flex flex-col">
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">User</div>
+                <div className="text-xl font-bold">
+                  {request.user.first_name} {request.user.last_name}
+                </div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Client</div>
+                <div className="text-xl font-bold">
+                  {request.client.first_name} {request.client.last_name}
+                </div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Agency</div>
+                <div className="text-xl font-bold">{request.agency.name}</div>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+        <div className="py-4" />
+        <Card className="w-2/3">
+          <CardHeader>
+            <CardTitle className="flex flex-col text-2xl font-extralight">
+              Requested Funds
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="border border-2 py-4 mx-4 rounded-lg">
+            <div className="flex flex-col">
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Fund</div>
+                <div className="text-xl font-bold">
+                  {request.user.first_name} {request.user.last_name}
+                </div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Client</div>
+                <div className="text-xl font-bold">
+                  {request.client.first_name} {request.client.last_name}
+                </div>
+              </div>
+              <Separator className="my-2" />
+              <div className="flex flex-cols-2 justify-between">
+                <div className="text-xl font-extralight">Agency</div>
+                <div className="text-xl font-bold">{request.agency.name}</div>
               </div>
             </div>
           </CardContent>
