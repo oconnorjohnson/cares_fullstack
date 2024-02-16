27608ddfde6eb259606e99243f327101816d0b0e
Daniel Johnson <dojohnson@ucdavis.edu>
2024-02-16 15:34:51 -0800

    added new cards and fixed spacingt on user/dashboard

--- components/user/dashboard/user-dashboard.tsx
+++ components/user/dashboard/user-dashboard.tsx
@@ -30,41 +30,70 @@ export default function Dashboard() {
       <>
         <div className="flex flex-row">
           <SideNavBar />
-          <div className="flex border-t flex-col w-5/6">
+          <div className="flex flex-col border-t w-5/6">
             <div className="text-3xl font-bold pl-12 pt-12">My Dashboard</div>
-            <div className="flex flex-grow space-x-8 px-12 pt-6">
-              <div className="flex flex-col flex-grow flex-grow">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Open Requests</CardTitle>
-                  </CardHeader>
-                  <CardContent className="flex flex-row justify-start space-x-4 items-center">
-                    <ClockIcon />
-                    <div className="text-4xl font-black">45</div>
-                  </CardContent>
-                </Card>
+            {/* Wrap each row of cards in a container */}
+            <div className="flex flex-col space-y-8 px-12 pt-6">
+              {/* First row of cards */}
+              <div className="flex space-x-8">
+                <div className="flex flex-col flex-grow">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Open Requests</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                      <ClockIcon />
+                      <div className="text-4xl font-black">45</div>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Approved Requests</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                      <CheckCircleIcon />
+                      <div className="text-4xl font-black">12</div>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>Denied Requests</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                      <XCircleIcon />
+                      <div className="text-4xl font-black">2</div>
+                    </CardContent>
+                  </Card>
+                </div>
               </div>
-              <div className="flex flex-col flex-grow">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Approved Requests</CardTitle>
-                  </CardHeader>
-                  <CardContent className="flex flex-row justify-start space-x-4 items-center">
-                    <CheckCircleIcon />
-                    <div className="text-4xl font-black">12</div>
-                  </CardContent>
-                </Card>
-              </div>
-              <div className="flex flex-col flex-grow">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Denied Requests</CardTitle>
-                  </CardHeader>
-                  <CardContent className="flex flex-row justify-start space-x-4 items-center">
-                    <XCircleIcon />
-                    <div className="text-4xl font-black">2</div>
-                  </CardContent>
-                </Card>
+              {/* Second row of cards */}
+              <div className="flex space-x-8">
+                <div className="flex flex-col flex-grow">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>New Request</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                      <XCircleIcon />
+                      <div className="text-4xl font-black">2</div>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div className="flex flex-col flex-grow">
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>New Client</CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                      <XCircleIcon />
+                      <div className="text-4xl font-black">2</div>
+                    </CardContent>
+                  </Card>
+                </div>
               </div>
             </div>
           </div>
