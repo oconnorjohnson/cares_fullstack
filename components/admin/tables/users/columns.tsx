8ca7090c405a6759a2833fb4b6cae780d85befd0
Daniel Johnson <dojohnson@ucdavis.edu>
2024-02-15 16:28:01 -0800

    fixed

--- components/admin/tables/users/columns.tsx
+++ components/admin/tables/users/columns.tsx
@@ -74,45 +74,33 @@ export const columns: ColumnDef<User>[] = [
       );
     },
   },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const request = row.original;
+  // {
+  //   id: "actions",
+  //   cell: ({ row }) => {
+  //     const request = row.original;
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <Link
-              href="/admin/users/[userId]"
-              as={`/admin/users/${request.userId}`}
-            >
-              <DropdownMenuItem>View User</DropdownMenuItem>
-            </Link>
+  //     return (
+  //       <DropdownMenu>
+  //         <DropdownMenuTrigger asChild>
+  //           <Button variant="ghost" className="h-8 w-8 p-0">
+  //             <span className="sr-only">Open menu</span>
+  //             <MoreHorizontal className="h-4 w-4" />
+  //           </Button>
+  //         </DropdownMenuTrigger>
+  //         <DropdownMenuContent align="end">
+  //           <DropdownMenuLabel>Actions</DropdownMenuLabel>
+  //           <DropdownMenuSeparator />
+  //           <Link
+  //             href="/admin/users/[userId]"
+  //             as={`/admin/users/${request.userId}`}
+  //           >
+  //             <DropdownMenuItem>View User</DropdownMenuItem>
+  //           </Link>
 
-            <DropdownMenuSeparator />
-
-            {/* 
-            add function to dropdown menu item like:
-            onClick={() => navigator.clipboard.writeText(payment.id)}
-             */}
-            {/* <DropdownMenuItem className="bg-green-600 text-white">
-              Approve
-            </DropdownMenuItem>
-            <div className="py-0.5" />
-            <DropdownMenuItem className="bg-red-600 text-white">
-              Deny
-            </DropdownMenuItem> */}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
+  //           <DropdownMenuSeparator />
+  //         </DropdownMenuContent>
+  //       </DropdownMenu>
+  //     );
+  //   },
+  // },
 ];
