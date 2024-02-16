9c73cfe5bf087d236f060ad6f1e94a107b086c2d
Daniel Johnson <dojohnson@ucdavis.edu>
2024-02-16 15:23:29 -0800

    added cards to user dashbaord

--- components/user/dashboard/user-dashboard.tsx
+++ components/user/dashboard/user-dashboard.tsx
@@ -8,18 +8,16 @@ import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 import { FreshPageAlert } from "@/components/shared/alerts";
 import SideNavBar from "@/components/user/dashboard/side-nav";
-
 import {
-  HomeIcon,
-  GitPullRequestIcon,
-  UsersIcon,
-  DollarSignIcon,
-  BarChart2,
-  MailIcon,
-  SettingsIcon,
-  LogOutIcon,
-  SendIcon,
-} from "lucide-react";
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { ClockIcon, CheckCircleIcon, XCircleIcon } from "lucide-react";
 import { SignOutButton } from "@clerk/nextjs";
 
 export default function Dashboard() {
@@ -33,8 +31,42 @@ export default function Dashboard() {
         <div className="flex flex-row">
           <SideNavBar />
           <div className="flex border-t flex-col w-5/6">
-            <div className="flex flex-row justify-between py-6"></div>
-            <div className="text-3xl font-bold pl-12">My Dashboard</div>
+            <div className="text-3xl font-bold pl-12 pt-12">My Dashboard</div>
+            <div className="flex flex-grow space-x-8 px-12 pt-6">
+              <div className="flex flex-col flex-grow flex-grow">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Open Requests</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                    <ClockIcon />
+                    <div className="text-4xl font-black">45</div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="flex flex-col flex-grow">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Approved Requests</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                    <CheckCircleIcon />
+                    <div className="text-4xl font-black">12</div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="flex flex-col flex-grow">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Denied Requests</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex flex-row justify-start space-x-4 items-center">
+                    <XCircleIcon />
+                    <div className="text-4xl font-black">2</div>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
           </div>
         </div>
       </>
