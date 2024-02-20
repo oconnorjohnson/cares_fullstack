cc8edb8bf75929830a3931fb8bbf1711f80cd2f3
Daniel Johnson <dojohnson@ucdavis.edu>
2024-02-20 15:40:45 -0800

    added base files and components for subitted, denied, approved, awaiting, reminder, late and banned email components

---
+++ components/emails/denied.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import {
+  Body,
+  Tailwind,
+  Button,
+  Container,
+  Head,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+
+interface EmailTemplateProps {
+  firstName: string;
+}
+
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+  firstName,
+}) => (
+  <Html lang="en">
+    <Preview>This is a test email.</Preview>
+    <Tailwind
+      config={{
+        theme: {
+          extend: {
+            colors: {
+              brand: "#007291",
+            },
+          },
+        },
+      }}
+    >
+      <Head />
+      <Body className="">
+        <Container className="">
+          <Section className="bg-zinc-200 rounded-2xl px-12 py-8">
+            <Text className="text-3xl font-bold">Hi {firstName},</Text>
+            <Text className="text-lg">
+              Welcome to the Yolo County Public Defender CARES program! We are
+              excited to work with you to help you get back on your feet. Please
+              click the button below to get started.
+            </Text>
+            <Button
+              className="bg-orange-500 p-4 rounded-xl text-white dark:text-white text-xl font-bold"
+              href="https://yolopublicdefendercares.org"
+            >
+              Get Started
+            </Button>
+          </Section>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
+);
