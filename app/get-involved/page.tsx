de7d8d2fbd291d11f8ece9b65e6750a1dd194876
Daniel Johnson <dojohnson@ucdavis.edu>
2024-02-22 15:25:11 -0800

    added more base structure to /get-help and /get-involved

--- app/get-involved/page.tsx
+++ app/get-involved/page.tsx
@@ -4,8 +4,16 @@ export default function GetInvolved() {
       <div className="flex flex-col">
         <div className="text-4xl font-bold text-center">Get Involved</div>
         <div className="flex flex-cols-2 justify-between mx-36">
-          <div>A div</div>
-          <div>A div</div>
+          <div className="w-1/2">An SVG illustration</div>
+          <div className="w-1/2 flex flex-col text-end">
+            <div>Supporting the journey to health and safety.</div>
+            <div>
+              The mission of CARES is to enhance the ability of the Yolo County
+              Public Defender&apos;s Office to address the unmet social,
+              financial, and behavioral health cares needs of individuals
+              navigating the justice system.
+            </div>
+          </div>
         </div>
       </div>
     </>
