c06b0a1349e88290f857db06e5d3e55d74bf22dc
Daniel Johnson <dojohnson@ucdavis.edu>
2024-04-05 16:37:30 -0700

    updated types_db.ts

--- types_db.ts
+++ types_db.ts
@@ -7,31 +7,6 @@ export type Json =
   | Json[];
 
 export type Database = {
-  graphql_public: {
-    Tables: {
-      [_ in never]: never;
-    };
-    Views: {
-      [_ in never]: never;
-    };
-    Functions: {
-      graphql: {
-        Args: {
-          operationName?: string;
-          query?: string;
-          variables?: Json;
-          extensions?: Json;
-        };
-        Returns: Json;
-      };
-    };
-    Enums: {
-      [_ in never]: never;
-    };
-    CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
   public: {
     Tables: {
       AdminEmailPrefs: {
@@ -552,6 +527,9 @@ export type Database = {
       };
       Request: {
         Row: {
+          adminOne: string | null;
+          adminThree: string | null;
+          adminTwo: string | null;
           agencyId: number;
           agreementUrl: string | null;
           approved: boolean;
@@ -576,6 +554,9 @@ export type Database = {
           userId: string;
         };
         Insert: {
+          adminOne?: string | null;
+          adminThree?: string | null;
+          adminTwo?: string | null;
           agencyId: number;
           agreementUrl?: string | null;
           approved?: boolean;
@@ -600,6 +581,9 @@ export type Database = {
           userId: string;
         };
         Update: {
+          adminOne?: string | null;
+          adminThree?: string | null;
+          adminTwo?: string | null;
           agencyId?: number;
           agreementUrl?: string | null;
           approved?: boolean;
@@ -624,6 +608,27 @@ export type Database = {
           userId?: string;
         };
         Relationships: [
+          {
+            foreignKeyName: "public_Request_adminOne_fkey";
+            columns: ["adminOne"];
+            isOneToOne: false;
+            referencedRelation: "User";
+            referencedColumns: ["userId"];
+          },
+          {
+            foreignKeyName: "public_Request_adminThree_fkey";
+            columns: ["adminThree"];
+            isOneToOne: false;
+            referencedRelation: "User";
+            referencedColumns: ["userId"];
+          },
+          {
+            foreignKeyName: "public_Request_adminTwo_fkey";
+            columns: ["adminTwo"];
+            isOneToOne: false;
+            referencedRelation: "User";
+            referencedColumns: ["userId"];
+          },
           {
             foreignKeyName: "public_Request_agencyId_fkey";
             columns: ["agencyId"];
@@ -813,185 +818,6 @@ export type Database = {
     };
     Functions: {
       [_ in never]: never;
-    };
-    Enums: {
-      [_ in never]: never;
-    };
-    CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-  storage: {
-    Tables: {
-      buckets: {
-        Row: {
-          allowed_mime_types: string[] | null;
-          avif_autodetection: boolean | null;
-          created_at: string | null;
-          file_size_limit: number | null;
-          id: string;
-          name: string;
-          owner: string | null;
-          owner_id: string | null;
-          public: boolean | null;
-          updated_at: string | null;
-        };
-        Insert: {
-          allowed_mime_types?: string[] | null;
-          avif_autodetection?: boolean | null;
-          created_at?: string | null;
-          file_size_limit?: number | null;
-          id: string;
-          name: string;
-          owner?: string | null;
-          owner_id?: string | null;
-          public?: boolean | null;
-          updated_at?: string | null;
-        };
-        Update: {
-          allowed_mime_types?: string[] | null;
-          avif_autodetection?: boolean | null;
-          created_at?: string | null;
-          file_size_limit?: number | null;
-          id?: string;
-          name?: string;
-          owner?: string | null;
-          owner_id?: string | null;
-          public?: boolean | null;
-          updated_at?: string | null;
-        };
-        Relationships: [];
-      };
-      migrations: {
-        Row: {
-          executed_at: string | null;
-          hash: string;
-          id: number;
-          name: string;
-        };
-        Insert: {
-          executed_at?: string | null;
-          hash: string;
-          id: number;
-          name: string;
-        };
-        Update: {
-          executed_at?: string | null;
-          hash?: string;
-          id?: number;
-          name?: string;
-        };
-        Relationships: [];
-      };
-      objects: {
-        Row: {
-          bucket_id: string | null;
-          created_at: string | null;
-          id: string;
-          last_accessed_at: string | null;
-          metadata: Json | null;
-          name: string | null;
-          owner: string | null;
-          owner_id: string | null;
-          path_tokens: string[] | null;
-          updated_at: string | null;
-          version: string | null;
-        };
-        Insert: {
-          bucket_id?: string | null;
-          created_at?: string | null;
-          id?: string;
-          last_accessed_at?: string | null;
-          metadata?: Json | null;
-          name?: string | null;
-          owner?: string | null;
-          owner_id?: string | null;
-          path_tokens?: string[] | null;
-          updated_at?: string | null;
-          version?: string | null;
-        };
-        Update: {
-          bucket_id?: string | null;
-          created_at?: string | null;
-          id?: string;
-          last_accessed_at?: string | null;
-          metadata?: Json | null;
-          name?: string | null;
-          owner?: string | null;
-          owner_id?: string | null;
-          path_tokens?: string[] | null;
-          updated_at?: string | null;
-          version?: string | null;
-        };
-        Relationships: [
-          {
-            foreignKeyName: "objects_bucketId_fkey";
-            columns: ["bucket_id"];
-            isOneToOne: false;
-            referencedRelation: "buckets";
-            referencedColumns: ["id"];
-          },
-        ];
-      };
-    };
-    Views: {
-      [_ in never]: never;
-    };
-    Functions: {
-      can_insert_object: {
-        Args: {
-          bucketid: string;
-          name: string;
-          owner: string;
-          metadata: Json;
-        };
-        Returns: undefined;
-      };
-      extension: {
-        Args: {
-          name: string;
-        };
-        Returns: string;
-      };
-      filename: {
-        Args: {
-          name: string;
-        };
-        Returns: string;
-      };
-      foldername: {
-        Args: {
-          name: string;
-        };
-        Returns: string[];
-      };
-      get_size_by_bucket: {
-        Args: Record<PropertyKey, never>;
-        Returns: {
-          size: number;
-          bucket_id: string;
-        }[];
-      };
-      search: {
-        Args: {
-          prefix: string;
-          bucketname: string;
-          limits?: number;
-          levels?: number;
-          offsets?: number;
-          search?: string;
-          sortcolumn?: string;
-          sortorder?: string;
-        };
-        Returns: {
-          name: string;
-          id: string;
-          updated_at: string;
-          created_at: string;
-          last_accessed_at: string;
-          metadata: Json;
-        }[];
-      };
     };
     Enums: {
       [_ in never]: never;
