d1430475b5a0c94b5b45f57de5ee33b19fdf3239
Daniel Johnson <dojohnson@ucdavis.edu>
2024-07-12 11:33:22 -0700

    Asdf

--- components/admin/dashboard/pre-post-analysis.tsx
+++ components/admin/dashboard/pre-post-analysis.tsx
@@ -63,7 +63,7 @@ export default function PrePostAnalysis({ chartData }: PrePostAnalysisProps) {
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
-          className="mx-auto aspect-square max-h-[400px]"
+          className="mx-auto aspect-square max-h-[300px]"
           config={chartConfig}
         >
           <BarChart accessibilityLayer data={transformedChartData}>
--- components/admin/dashboard/reqs-by-agency.tsx
+++ components/admin/dashboard/reqs-by-agency.tsx
@@ -88,7 +88,7 @@ export default function RequestsByAgency({
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
-          className="mx-auto aspect-square max-h-[400px]"
+          className="mx-auto aspect-square max-h-[300px]"
           config={chartConfig}
         >
           <PieChart>
