d3757a7562fbe2e387e28d8d90517a62ba66e71c
Daniel Johnson <dojohnson@ucdavis.edu>
2024-07-18 17:00:56 -0700

    added more to do notes

--- TODO.md
+++ TODO.md
@@ -12,7 +12,13 @@ Add items, add selected checkbox once completed and move into 'DONE'.
 - [ ] export data from DB into CSV for comparisons
 - [ ] add a chart to compare race and sex of clients
 - [ ] double check the circled requests on Tracie's papers and compare with paper requests from Emily -- couple discrepancies that we need to confirm are correct discrepancies
+- [ ] add specific frontend error handling in all client forms first, then move on to admin forms. New client, new request, etc. For new request, if we can show errors on previous tabs until clicking submit render all errors to last tab and say what the error is and which tab it is on. Handle errors related to overly long "additional information" and other fields in pre and post screen. Look into better form handling...right now when you click submit and then click the form again the old data is still there, at least for some of the forms maybe not all of them.
 - [ ] log in to amplifund and start recording new requests after we bring old requests up to date
+- [ ] table to view paid funds with following information:
+  - Fund Type
+  - Amount
+  - Data
+  - if fund type id is 3 then display (amount \* 2.5)
 
 [] import { getAllRequests } from "@/server/actions/request/action" into "@/components/admin/tables/requests/page.tsx"
 
--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,5 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { auth } from "@clerk/nextjs/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import {
   createUser,
@@ -14,7 +13,6 @@ import {
 import { deleteUser } from "@/server/supabase/functions/delete";
 import { EmailTemplate } from "@/components/emails/welcome";
 import { Resend } from "resend";
-import { Tables } from "@/types_db";
 
 interface EmailAddress {
   email_address: string;
@@ -28,9 +26,11 @@ interface UserData {
   last_name: string;
   public_metadata: any;
 }
+
 const resend = new Resend(process.env.RESEND_API_KEY);
+
 async function sendWelcomeEmail(firstName: string, email: string) {
-  console.log(`Attempting to send welcome email to ${email}`);
+  // console.log(`Attempting to send welcome email to ${email}`);
   const { data, error } = await resend.emails.send({
     from: "CARES <info@yolopublicdefendercares.org>",
     to: [email],
@@ -39,73 +39,65 @@ async function sendWelcomeEmail(firstName: string, email: string) {
       firstName: firstName,
     }) as React.ReactElement,
   });
-
   if (error) {
-    console.error("Error from Resend API:", error);
+    // console.error("Error from Resend API:", error);
     throw new Error(`Error sending welcome email: ${error}`);
   }
-
-  console.log("Success response from Resend API:", data);
+  // console.log("Success response from Resend API:", data);
   return data;
 }
 
 export async function POST(req: Request) {
-  console.log("Starting POST function");
+  // console.log("Starting POST function");
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
   if (!WEBHOOK_SECRET) {
-    console.error("WEBHOOK_SECRET not set.");
+    // console.error("WEBHOOK_SECRET not set.");
     throw new Error("WEBHOOK_SECRET not set.");
   }
-  console.log("Extracting headers");
+  // console.log("Extracting headers");
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
-  console.log(
-    `Received svix_id: ${svix_id}, svix_timestamp: ${svix_timestamp}, svix_signature: ${svix_signature}`,
-  );
+  // console.log(
+  //   `Received svix_id: ${svix_id}, svix_timestamp: ${svix_timestamp}, svix_signature: ${svix_signature}`,
+  // );
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    console.error("Error occurred -- no svix headers");
+    // console.error("Error occurred -- no svix headers");
     return new Response("Error occured -- no svix headers", {
       status: 400,
     });
   }
-
   const payload = await req.json();
   const body = JSON.stringify(payload);
-  console.log("Payload received:", payload);
+  // console.log("Payload received:", payload);
   const wh = new Webhook(WEBHOOK_SECRET);
-
   let evt: WebhookEvent;
-
   try {
-    console.log("Verifying webhook");
+    // console.log("Verifying webhook");
     evt = wh.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
     }) as WebhookEvent;
-    console.log("Webhook verified successfully");
+    // console.log("Webhook verified successfully");
   } catch (err) {
-    console.error("Error verifying webhook:", err);
+    // console.error("Error verifying webhook:", err);
     return new Response("Error occured", {
       status: 400,
     });
   }
-
   const { id } = evt.data;
   const eventType = evt.type;
   console.log(`Processing event type: ${eventType} with ID: ${id}`);
-
   switch (eventType) {
     case "user.created": {
-      console.log("User creation event detected");
+      // console.log("User creation event detected");
       const { id, email_addresses, first_name, last_name } =
         evt.data as unknown as UserData;
-      console.log(
-        `User details: ID=${id}, First Name=${first_name}, Last Name=${last_name}, Email Addresses=${email_addresses.length}`,
-      );
+      // console.log(
+      //   `User details: ID=${id}, First Name=${first_name}, Last Name=${last_name}, Email Addresses=${email_addresses.length}`,
+      // );
       const userData = {
         userId: id,
         first_name,
@@ -115,26 +107,23 @@ export async function POST(req: Request) {
         userId: id,
         email: emailAddress.email_address,
       }));
-      console.log("Creating user in database");
+      // console.log("Creating user in database");
       const user = await createUser(userData);
-      console.log(`User ${user} created successfully`);
-
+      // console.log(`User ${user} created successfully`);
       const emails = await createEmailAddresses(emailData);
-      console.log(`Emails ${emails} created successfully`);
-
+      // console.log(`Emails ${emails} created successfully`);
       if (email_addresses.length > 0) {
         const primaryEmail = email_addresses[0].email_address;
         await sendWelcomeEmail(first_name, primaryEmail);
-        console.log(`Welcome email sent to ${primaryEmail}`);
+        // console.log(`Welcome email sent to ${primaryEmail}`);
       }
       break;
     }
     case "user.updated": {
-      console.log("User update event detected");
+      // console.log("User update event detected");
       const { id, first_name, last_name, email_addresses, public_metadata } =
         evt.data as UserData;
-      console.log(`Updating user: ID=${id}`);
-
+      // console.log(`Updating user: ID=${id}`);
       const userData = {
         userId: id,
         first_name,
@@ -145,39 +134,37 @@ export async function POST(req: Request) {
         userData.public_metadata &&
         userData.public_metadata.admin === "true"
       ) {
-        console.log(
-          `User ${userData.userId} is an admin. Updating admin status.`,
-        );
+        // console.log(
+        //   `User ${userData.userId} is an admin. Updating admin status.`,
+        // );
         await updateAdminUser(userData.userId);
-        console.log("adding admin email preference record");
+        // console.log("adding admin email preference record");
         const addedRecord = await createAdminEmailPreferenceRecord(
           userData.userId,
         );
-        console.log(
-          `Admin email preference record added successfully: ${addedRecord}`,
-        );
+        // console.log(
+        //   `Admin email preference record added successfully: ${addedRecord}`,
+        // );
       }
-      console.log("Updating user in database with new details");
+      // console.log("Updating user in database with new details");
       const user = await updateUser(id, userData);
-      console.log(`User ${user} updated`);
+      // console.log(`User ${user} updated`);
       break;
     }
     case "user.deleted": {
-      console.log("User deletion event detected");
+      // console.log("User deletion event detected");
       const { id } = evt.data as unknown as UserData;
-      console.log(`Deleting user: ID=${id}`);
+      // console.log(`Deleting user: ID=${id}`);
       await deleteUser(id);
-      console.log(`User ${id} deleted successfully`);
+      // console.log(`User ${id} deleted successfully`);
       break;
     }
     default: {
-      console.log(`Unhandled event type: ${eventType}`);
+      // console.log(`Unhandled event type: ${eventType}`);
     }
   }
-
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
-
+  // console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  // console.log("Webhook body:", body);
   return new Response("", { status: 200 });
 }
 export const runtime = "edge";
