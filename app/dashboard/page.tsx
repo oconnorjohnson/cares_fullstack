e8c23dc33228499029b6b895c020717219601113
Daniel Johnson <dojohnson@ucdavis.edu>
2024-08-02 18:58:29 -0700

    added loading fallback with react suspense on dashboard/page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,22 @@
 import { auth, SignedIn } from "@clerk/nextjs";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const UserDashboard = dynamic(
   () => import("@/components/user/dashboard/user-dashboard"),
+  { loading: () => <LoadingFallback /> },
 );
 const AdminDashboard = dynamic(
   () => import("@/components/admin/dashboard/admin-dashboard"),
+  { loading: () => <LoadingFallback /> },
 );
-
+function LoadingFallback() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+  );
+}
 export default function Home() {
   const { sessionClaims } = auth();
   const isAdmin = (sessionClaims?.publicMetadata as any)?.admin;
